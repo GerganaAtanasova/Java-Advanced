246321ac1c2f80f359dab5071993450b958e0e23
GerganaAtanasova <gergana.atanasova3333@gmail.com>
2022-04-24 17:14:10 +0300

    polimorphism

---
+++ polimorphism/Problem03/shapes/Circle.java
@@ -0,0 +1,23 @@
+package polymorphism.shapes;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public final Double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
---
+++ polimorphism/Problem04/animals/Dog.java
@@ -0,0 +1,16 @@
+package polymorphism.animals;
+
+public class Dog extends Animal{
+
+
+    public Dog(String name, String favouriteFood){
+        super(name,favouriteFood);
+    }
+
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%n",
+                getName(),getFavouriteFood()) + "DJAAF";
+    }
+}
